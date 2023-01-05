0e595a6d853ca874a858fcf6387717067dc0046d
Zac Smart <zac_chaz@outlook.com>
2023-01-05 14:53:25 +1100

    pushing mutation adds

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,4 +11,45 @@ mutation loginUser($email: String!, $password: String!) {
 }
 `;
 
-export const 
+export const ADD_USER =qgl`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                bookId
+                image
+                link
+                title
+                description
+            }
+        }
+        token
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks{
+            # _id
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}
+`;
+
