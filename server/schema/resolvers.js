c5cacf31657738c2d006b3f5ca1940909074ae2b
Zac Smart <zac_chaz@outlook.com>
2023-01-05 12:33:57 +1100

    changes to resolver mutation

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -15,5 +15,25 @@ const resolvers = {
             return foundUser;
         },
     },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            if (!user) {
+                throw new AuthErr ('Whoops!! somthing went wrong!');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password}) => {
+            const user = await User.findOne ({ email });
+            if (!user) {
+                throw new AuthErr('No user with this email found');
+            }
+            const correctPassword = await user.isCorrectPassword(password);
+            if (!correctPassword) {
+                throw new AuthErr('Incorrect Credentials');
+            }
+        }
+    }
     
 }
