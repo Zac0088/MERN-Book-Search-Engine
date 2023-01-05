ca94cf9bee03beaea2a86697f4fe97f8b088353f
Zac Smart <zac_chaz@outlook.com>
2023-01-05 13:51:09 +1100

    working on mutations

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -3,17 +3,44 @@ const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    me: async (parent, args, context) => {
-        if (context.user) {
-            const userData = await User.findOne({})
-            .select('-__v -password')
-            .populate('books');
-            return userData;
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                    .select('-__v -password')
+                    .populate('books');
+                return userData;
+            }
+            throw new AuthErr('You are not logged in');
+        },
+       },  
+
+        Mutation: {
+            addUser: async (parent, args) => {
+                const user = await User.create(args);
+                const token = signToken(user);
+                return { token, user };
+            },
+            login: async (parent, {email, password}) => {
+                const user = await User.findOne({ email });
+                if (!user) {
+                    throw new AuthErr('Incorrect Credentials');
+                }
+                const token =signToken(user);
+                return {token, user};
+            },
+            saveBook: async (parent, args, context) => {
+                if(context.user) {
+                    const updateUser = await User.findByIdAndUpdate(
+                        { _id: context.user._id },
+                        { $addToSet: { savedBooks: args.input } },
+                        {new: true }
+                    );
+                    return updateUser;
+                }
+                throw new AuthErr('You must be logged in');
+            }
         }
-        throw new AuthErr('You are not logged in');
-    },
-},
 
 
-
-module.exports = resolvers;
+// module.exports = resolvers;
